2977fd3ceb74c556b991928148dea5654fbba942
Kyle Lamocha <20190017027@my.xu.edu.ph>
2025-12-22 23:43:18 +0800

    new changes added

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test';
+import { test , expect } from '@playwright/test';
 const { webkit } = require('playwright'); 
 
 test('example test', async () => {
@@ -8,9 +8,16 @@ test('example test', async () => {
         // Create a new page (tab)
         const page = await browser.newPage();
         //go to link for screenshot
-        await page.goto("https://marvelcinematicuniverse.fandom.com/wiki/Rocket_Raccoon");
+         await page.goto("https://marvelcinematicuniverse.fandom.com/wiki/Rocket_Raccoon", {
+            waitUntil: "domcontentloaded",
+        });
         // Take a screenshot and save it to a file
-        await page.screenshot({ path: "example-test.png"});
+        await page.screenshot({ path: "example-test.png", 
+        });
+
+        // verify if screenshot matches
+        await expect(page).toHaveScreenshot('example-test.png');
+
     } catch (e) {
         console.log(e);  //This line will execute and log the Error object
     } finally {
